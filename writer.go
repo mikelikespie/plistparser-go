c9ac0a1a81dcf233653aa16e59816543e65af335
Mike Lewis <mikelikespie@gmail.com>
2015-05-27 22:20:09 -0700

    Using reflect to write out dictionaries

--- writer.go
+++ writer.go
@@ -5,6 +5,8 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"log"
+	"reflect"
 	"sort"
 	"strconv"
 )
@@ -39,6 +41,8 @@ func quoteStringIfNeeded(str string) string {
 	return str
 }
 
+var stringType = reflect.TypeOf("")
+
 func write(val interface{}, writer *bufio.Writer, indent int) (err error) {
 	writeIndent := func() {
 		for i := 0; i < indent; i++ {
@@ -47,55 +51,11 @@ func write(val interface{}, writer *bufio.Writer, indent int) (err error) {
 	}
 
 	switch val := val.(type) {
-	case []interface{}:
-		if _, err = writer.WriteString("(\n"); err != nil {
-			return
-		}
-
-		indent++
-		for _, v := range val {
-			writeIndent()
-			write(v, writer, indent)
-			writer.WriteString(",\n")
-		}
-		indent--
-
-		writeIndent()
-		if _, err = writer.WriteString(")"); err != nil {
-			return
-		}
-
-	case map[string]interface{}:
-		if _, err = writer.WriteString("{\n"); err != nil {
-			return
-		}
-		var keys []string
-		for k := range val {
-			keys = append(keys, k)
-		}
-
-		sort.Strings(keys)
-
-		indent++
-		for _, k := range keys {
-			v := val[k]
-			writeIndent()
-			writer.WriteString(quoteStringIfNeeded(k))
-			writer.WriteString(" = ")
-			write(v, writer, indent)
-			writer.WriteString(";\n")
-		}
-		indent--
-
-		writeIndent()
-		if _, err = writer.WriteString("}"); err != nil {
-			return
-		}
-
 	case string:
 		if _, err = writer.WriteString(quoteStringIfNeeded(val)); err != nil {
 			return
 		}
+		return
 	case []byte:
 		if _, err = writer.WriteRune('<'); err != nil {
 			return
@@ -116,6 +76,72 @@ func write(val interface{}, writer *bufio.Writer, indent int) (err error) {
 		if _, err = writer.WriteRune('>'); err != nil {
 			return
 		}
+		return
+	}
+
+	reflectVal := reflect.ValueOf(val)
+
+	t := reflectVal.Type()
+	switch t.Kind() {
+	case reflect.Slice:
+		if _, err = writer.WriteString("(\n"); err != nil {
+			return
+		}
+
+		indent++
+		l := reflectVal.Len()
+		for i := 0; i < l; i++ {
+			writeIndent()
+			write(reflectVal.Index(i).Interface(), writer, indent)
+			writer.WriteString(",\n")
+		}
+
+		indent--
+
+		writeIndent()
+		if _, err = writer.WriteString(")"); err != nil {
+			return
+		}
+
+		return
+	case reflect.Map:
+		keyType := t.Key()
+		if !keyType.ConvertibleTo(stringType) {
+			log.Panicln("Can't have key that isn't convertable to a string type")
+		}
+
+		if _, err = writer.WriteString("{\n"); err != nil {
+			return
+		}
+
+		var keys []string
+		for _, k := range reflectVal.MapKeys() {
+			keys = append(keys, k.Convert(stringType).Interface().(string))
+		}
+
+		sort.Strings(keys)
+
+		indent++
+		for _, k := range keys {
+			v := reflectVal.MapIndex(reflect.ValueOf(k).Convert(keyType)).Interface()
+			writeIndent()
+			writer.WriteString(quoteStringIfNeeded(k))
+			writer.WriteString(" = ")
+			write(v, writer, indent)
+			writer.WriteString(";\n")
+		}
+		indent--
+
+		writeIndent()
+		if _, err = writer.WriteString("}"); err != nil {
+			return
+		}
+	case reflect.String:
+		if _, err = writer.WriteString(quoteStringIfNeeded(reflectVal.Convert(stringType).Interface().(string))); err != nil {
+			return
+		}
+
+		return
 	default:
 		err = fmt.Errorf("Could not serialize type of value %+v", val)
 		return
